21dc9091aad17981eb3d50914c6216b07f10d05e
Diana Nguyen <dna44@sfu.ca>
2025-06-10 16:02:38 -0700

    feature: added inventory bar and resume
    inventory bar is added with hover details and
    resume placeholder, but resume functionality not added
    yet. Next add functionality.
    
    GitHub Issue #8: Done

--- react-portfolio/src/components/MainScreen/MainScreen.jsx
+++ react-portfolio/src/components/MainScreen/MainScreen.jsx
@@ -4,12 +4,42 @@ import React from 'react';
 import './MainScreen.css';
 
 function MainScreen() {
+
+    const inventorySlots = [
+        {type: 'resume', content: 'Resume'},
+        {type: 'empty'},
+        {type: 'empty'},
+        {type: 'empty'},
+        {type: 'empty'},
+        {type: 'empty'},
+        {type: 'empty'},
+        {type: 'empty'},
+        {type: 'empty'},
+        {type: 'empty'}
+    ]
+
     return (
         <div className="MainScreenContainer">
+
+            <div className="TopSpacer"></div>
+
             <div className="MainTextContainer">
                 <h1 className="MainText">Hello, my name is Diana!</h1>
                 <p className="Quote">Making play a profession.</p>
             </div>
+
+            <div className="InventoryBarContainer">
+                {inventorySlots.map((slot, index) => (
+                    <div key={index} className={`InventorySlot ${slot.type == 'resume' ? 'resumeSlot' : ''}`}>
+                        {slot.content && (
+                            <span className="slotContent">
+                                {slot.content}
+                            </span>
+                        )}
+                    </div>
+                ))}
+            </div>
+
         </div>
     );
 }
