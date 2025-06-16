b575cd92cf53550395dbfc9f5003254172f900af
Diana Nguyen <dna44@sfu.ca>
2025-06-16 00:17:04 -0700

    Forgot to commit skills overlay code

---
+++ react-portfolio/src/sections/Resume/Resume.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import './Resume.css';
+
+function Resume({ resumePDFPath }) {
+    return(
+        <div className="ResumeOverlay">
+            <div className="ResumeContent">
+
+                <iframe src={resumePDFPath} title="My Resume" className="ResumePDFViewer" frameborder="0">
+                    <p>Your browser does not support PDFs. <a href={resumePDFPath} download>Download PDF</a></p>
+                </iframe>
+
+                <a href={resumePDFPath} download className="ResumeDownloadButton">Download Resume</a>
+
+            </div>
+        </div>
+    );
+}
+
+export default Resume;
