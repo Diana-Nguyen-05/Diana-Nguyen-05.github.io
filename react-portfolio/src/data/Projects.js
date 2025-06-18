08b82dfa542441974b3aa3be97bc22b6c3260ee2
Diana Nguyen <dna44@sfu.ca>
2025-06-17 23:58:55 -0700

    Added data info for my projects

---
+++ react-portfolio/src/data/Projects.js
@@ -0,0 +1,32 @@
+
+const projects = [
+   {
+    id: 'project1',
+    title: 'Snake Web Game',
+    description: 'The classic snake game developed with HTML, CSS, and JS.',
+    image: '/images/SnakeWebGame.png',
+    technologies: ['Html', 'Css', 'Javascript'],
+    githubLink: 'https://github.com/Diana-Nguyen-05/Snake-Game-Website.git',
+    LiveDemoLink: null,
+   },
+   {
+    id: 'project2',
+    title: 'Movie Site Clone',
+    description: 'Built from a youtube tutorial, this is a front-end movie site clone.',
+    image: '/images/MovieSiteClone.png',
+    technologies: ['Html', 'Css', 'Javascript'],
+    githubLink: 'https://github.com/Diana-Nguyen-05/Movie-Site-Clone.git',
+    LiveDemoLink: null,
+   },
+   {
+    id: 'project3',
+    title: 'Panorama Maker',
+    description: 'School Assignment: Takes in images of different angles as an input, and outputs a panorama of those images.',
+    image: '/images/PanoramaCreator.png',
+    technologies: ['Matlab'],
+    githubLink: 'https://github.com/Diana-Nguyen-05/Panorama-Creator.git',
+    LiveDemoLink: null,
+   },
+];
+
+export default projects;
